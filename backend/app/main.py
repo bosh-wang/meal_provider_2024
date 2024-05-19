9fb646ab13a656313d29ff0a6ba65aba896fcdfb
pohsunwang <96949018+bosh-wang@users.noreply.github.com>
2024-05-19 06:58:28 +0000

    feat: Add payment notification API endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 
-from services import rating_service, order_history_service
+from services import rating_service, order_history_service, payment_service
 
 app = Flask(__name__)
 
@@ -14,17 +14,23 @@ def get_order_history_service():
     data = order_history_service.get_order_history_service()#request.get_json()
     return jsonify(data)
 
-
+# get rating
 @app.route('/api/getRating', methods=['GET'])
 def get_rating():
     data = rating_service.get_rating_service()#request.get_json()
     return jsonify(data)
 
+# update rating
 @app.route('/api/updateRating', methods=['POST'])
 def update_rating():
     data = rating_service.update_rating_service()#request.get_json()
     return jsonify(data)
 
+# payment notification
+@app.route('/api/paymentNotification', methods=['GET'])
+def payment_notiy():
+    data = payment_service.payment_notification_service()#request.get_json()
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ backend/app/services/payment_service.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+import mysql.connector
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def payment_notification_service():#data
+
+    # conn = mysql.connector.connect(
+    # host='localhost',           
+    # user='user',               
+    # password='@MySQLPassword', 
+    # )
+    # cursor = conn.cursor()
+
+    # try:
+    #     item_id = data['user_id']
+
+    #     cursor.execute("SELECT orders.user_id, users.email, orders.total_amount FROM orders JOIN users ON orders.user_id=users.user_id group by orders.user_id")
+    #     orders = cursor.fetchall()
+
+    #     for user_id, email, total_amount in orders:
+    #         if total_amount > 0:
+    #             print(f"Sending email to {email} for payment of {total_amount}")
+    #             send_email(email, total_amount)
+
+    #     cursor.close()
+    #     return ({"message": "Notification sent successfully"})
+    # except Exception as e:
+    #     return ({"error": str(e)}), 500
+    return({"message": "Notification sent successfully"})
+
+
+def send_email(email, total_amount):
+    smtp_server = 'smtp.gmail.com'  
+    smtp_port = 587 
+    sender_email = 'meal_provider@gmail.com'
+    password = '@@meal_provider@@'
+
+    for receiver_email in email:
+        message = MIMEMultipart()
+        message['From'] = sender_email
+        message['To'] = receiver_email
+        message['Subject'] = '[Notification] You have a food pending payment of $' + str(total_amount)
+
+        body = 'Hi there!\n\nThis is a reminder that you have a pending payment of $' + str(total_amount) + ' for your recent food order. Please make the payment at your earliest convenience.\n\nThank you!'
+        message.attach(MIMEText(body, 'plain'))
+
+        try:
+        
+            server = smtplib.SMTP(smtp_server, smtp_port)
+            server.starttls()  
+            server.login(sender_email, password) 
+            text = message.as_string() 
+            server.sendmail(sender_email, receiver_email, text)
+            print('Email sent successfully!')
+        except Exception as e:
+            print(f'Failed to send email. Error: {e}')
+        finally:
+            server.quit()  
