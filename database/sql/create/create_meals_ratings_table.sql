f10a4218ee53e6846e52e9d1dd2b6726d3fc59d7
esther <ester6126@gmail.com>
2024-05-19 21:08:28 +0800

    [update] update meal ratings table in pg

--- database/sql/create/create_meals_ratings_table.sql
+++ database/sql/create/create_meals_ratings_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE meals_ratings (
-    rating_id SERIAL PRIMARY KEY,
+    rating_id VARCHAR(50) PRIMARY KEY,
     user_id VARCHAR(50) NOT NULL,
     item_id VARCHAR(50) NOT NULL,
     star_rating DECIMAL(2, 1) CHECK (star_rating >= 0 AND star_rating <= 5),
@@ -8,4 +8,4 @@ CREATE TABLE meals_ratings (
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (item_id) REFERENCES menus_items(item_id)
-)PARTITION BY RANGE (created_at);
+);
