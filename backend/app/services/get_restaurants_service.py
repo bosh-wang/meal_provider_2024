300fec00be63929e086ca562b216c82f8c61bc96
林旭容 <58764256+SharonBear@users.noreply.github.com>
2024-05-30 13:28:55 +0000

    cache_res

--- backend/app/services/get_restaurants_service.py
+++ backend/app/services/get_restaurants_service.py
@@ -73,9 +73,18 @@ import json
 from decimal import Decimal
 from datetime import datetime, date
 import os
+import redis
+from datetime import timedelta
+import time
 
 app = Flask(__name__)
-
+pool = redis.ConnectionPool(
+    host=os.getenv("REDIS_HOST"),
+    port=os.getenv("REDIS_PORT"),
+    db=0,
+    decode_responses=True,
+)
+redis_client = redis.Redis(connection_pool=pool)
 def get_db_connection():
     conn = psycopg2.connect(
         host=os.getenv("DB_HOST"),
@@ -98,10 +107,23 @@ def get_restaurant(data):
     print(data)
     campus = data.get('campus')
     try:
-        conn = get_db_connection()
-        cursor = conn.cursor(cursor_factory=RealDictCursor)
-
         if campus:
+            cache_key = f"restaurants:{campus}"
+            cache_expiry = timedelta(hours=1)
+
+            start_time = time.time()  # Start timing for Redis
+
+            # Check if the data is in Redis cache
+            cached_restaurant = redis_client.get(cache_key)
+            if cached_restaurant:
+                redis_time = time.time() - start_time  # Calculate Redis time
+                print(f"Cache hit, Time taken with Redis: {redis_time:.6f} seconds")
+                return app.response_class(
+                    response=cached_restaurant, status=200, mimetype="application/json"
+                )
+            start_time = time.time()  # Start timing for DB query
+            conn = get_db_connection()
+            cursor = conn.cursor(cursor_factory=RealDictCursor)
             print("有輸入")
             # Query to fetch restaurant_ids based on campus
             query = """
@@ -113,6 +135,23 @@ def get_restaurant(data):
             """
             cursor.execute(query, (campus,))
         else:
+            campus = "K"
+            cache_key = f"restaurants:{campus}"
+            cache_expiry = timedelta(hours=1)
+
+            start_time = time.time()  # Start timing for Redis
+
+            # Check if the data is in Redis cache
+            cached_restaurant = redis_client.get(cache_key)
+            if cached_restaurant:
+                redis_time = time.time() - start_time  # Calculate Redis time
+                print(f"Cache hit, Time taken with Redis: {redis_time:.6f} seconds")
+                return app.response_class(
+                    response=cached_restaurant, status=200, mimetype="application/json"
+                )
+            start_time = time.time()  # Start timing for DB query
+            conn = get_db_connection()
+            cursor = conn.cursor(cursor_factory=RealDictCursor)
             print("沒輸入")
             # Query to fetch all restaurants
             query = """
@@ -123,11 +162,14 @@ def get_restaurant(data):
             cursor.execute(query)
 
         restaurants = cursor.fetchall()
-
         cursor.close()
         conn.close()
-
+        db_time = time.time() - start_time  # Calculate DB query time
+        print(f"Time taken with DB: {db_time:.6f} seconds")
         json_result = json.dumps(restaurants, ensure_ascii=False, default=custom_json_serializer)
+
+        # Store the result in Redis cache
+        redis_client.setex(cache_key, cache_expiry, json_result)
 
         response = app.response_class(
             response=json_result,
