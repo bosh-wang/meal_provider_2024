b11fb782815ac1cc11835d7e59245858c6916ede
HsuanHaoinIIM <164992295+HsuanHaoinIIM@users.noreply.github.com>
2024-05-31 11:16:59 +0000

    [add]login-api

--- frontend/mealprovider2024/src/app/component/pages/login/login.component.ts
+++ frontend/mealprovider2024/src/app/component/pages/login/login.component.ts
@@ -2,24 +2,29 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Login } from '../../../shared/model/Login';
+import { ApiService } from '../../../services/api.service';
+import { HttpClient,HttpClientModule } from '@angular/common/http';
 import CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule,HttpClientModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
   loginForm: FormGroup;
   submitted = false;
-
-  constructor(private formBuilder: FormBuilder) {
+  loginObj:Login=new Login();
+  //constructor(private formBuilder: FormBuilder,private apiService:ApiService,private http: HttpClient) {
+  constructor(private formBuilder: FormBuilder,private apiService:ApiService,private http: HttpClient) {  
     const formModel = new Login();
+
     this.loginForm = this.formBuilder.group({
       email: [formModel.email, [Validators.required, Validators.email]],
-      password: [formModel.password, [Validators.required, Validators.minLength(6)]],
+      password: [formModel.password_hash, [Validators.required, Validators.minLength(6)]],
       role: [formModel.role, Validators.required]
     });
   }
@@ -30,7 +35,6 @@ export class LoginComponent {
 
   onSubmit() {
     this.submitted = true;
-
     // Stop here if form is invalid
     if (this.loginForm.invalid) {
       return;
@@ -41,11 +45,26 @@ export class LoginComponent {
     // Create the final form value object
     const formValue = {
       "email": this.loginForm.value.email,
-      "password_hash": passwordHash,
+      "password_hash": this.loginForm.value.password,
       "role": this.loginForm.value.role
     };
     // Process login
     console.log('Login successful');
     console.log(formValue);
+
+    this.loginObj.email=this.loginForm.value.email;
+    this.loginObj.role=this.loginForm.value.role;
+    this.loginObj.password_hash=this.loginForm.value.password;
+    //this.loginObj.password_hash=passwordHash;
+    this.apiService.login(this.loginObj).subscribe({
+      next: res => {
+        console.log(res);
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+
+    
   }
 }
--- frontend/mealprovider2024/src/app/services/api.service.ts
+++ frontend/mealprovider2024/src/app/services/api.service.ts
@@ -2,10 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { Login } from '../shared/model/Login';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
+  loginURL:string;
+  constructor(private http : HttpClient){
+    this.loginURL='http://35.224.128.24/api/signin';
+  }
+
+  login(logindata:Login):Observable<Login>{
+    return this.http.post<Login>(this.loginURL,logindata);
+  }
 
 }
