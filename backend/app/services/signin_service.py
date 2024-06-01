0364c5fd30be26c7f6c17b51f31e755e59a19ead
林旭容 <58764256+SharonBear@users.noreply.github.com>
2024-06-01 15:51:30 +0000

    sign_in:e回傳e_id/r回傳r_id

--- backend/app/services/signin_service.py
+++ backend/app/services/signin_service.py
@@ -21,7 +21,30 @@ def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
 
-def sign_in(r, cID, passw):
+def get_additional_info(user_id, role):
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+        additional_info = {}
+
+        if role == "restaurant_staff":
+            query = "SELECT staff_id, restaurant_id FROM restaurants_staffs WHERE user_id = %s"
+            cur.execute(query, (user_id,))
+            additional_info = cur.fetchone()
+
+        elif role == "employee":
+            query = "SELECT employee_id FROM employees WHERE user_id = %s"
+            cur.execute(query, (user_id,))
+            additional_info = cur.fetchone()
+
+        cur.close()
+        conn.close()
+        return additional_info
+
+    except Exception as e:
+        return {"error": str(e)}
+
+def sign_in(role, email, password):
     try:
         conn = get_db_connection()
         cur = conn.cursor(cursor_factory=RealDictCursor)
@@ -31,21 +54,28 @@ def sign_in(r, cID, passw):
         FROM users
         WHERE email = %s AND role = %s AND password_hash = %s;
         """
-        passw_hash = hash_password(passw)
-        cur.execute(select_query, (cID, r, passw_hash))
+        passw_hash = hash_password(password)
+        cur.execute(select_query, (email, role, passw_hash))
         result = cur.fetchone()
 
         if result:
             data = {"message": "Login Successful", "user_id": result["user_id"]}
+
+            additional_info = get_additional_info(result["user_id"], role)
+            if additional_info:
+                data.update(additional_info)
+
             response = jsonify(data)
             response.status_code = 200
         else:
             data = {"message": "Login failed"}
             response = jsonify(data)
             response.status_code = 401
+
         cur.close()
         conn.close()
         return response
+
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
